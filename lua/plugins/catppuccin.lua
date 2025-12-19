49e7a18642406126afd714fbefc5c5e0629146a4
Brendan Coen <brendan@sonic.net>
2025-12-18 18:12:24 -0800

    made progress, but broke something in init.lua. Saving before I break more.

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,3 +1,11 @@
-return { 
-	"catppuccin/nvim", name = "catppuccin", priority = 1000 
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,  -- load before other plugins
+  config = function()
+    require("catppuccin").setup({
+      flavour = "mocha",  -- latte, frappe, macchiato, mocha
+    })
+    vim.cmd.colorscheme("catppuccin")
+  end,
 }
