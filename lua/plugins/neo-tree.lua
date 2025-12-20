0fc098f2bff3cfe3ae0ebfcd83319355e56a1ed6
Brendan Coen <brendan@sonic.net>
2025-12-19 20:37:36 -0800

    tweak neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,19 +1,22 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
+  'nvim-neo-tree/neo-tree.nvim',
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons',
+    'MunifTanjim/nui.nvim',
   },
   keys = {
-	  { "<C-n>", "<cmd>Neotree toggle<cr>", desc = "Toggle Neo-tree" },
+    { '<C-n>', '<cmd>Neotree toggle<cr>', desc = 'Toggle Neo-tree' },
   },
   config = function()
-    require("neo-tree").setup({
+    require('neo-tree').setup {
+      enable_git_status = true,
+      close_if_last_window = true,
+      sort_case_insensitive = true,
       window = {
-        position = "right",  -- "left", "right", "top", "bottom", "float", "current"
-	width = "25%",
+        position = 'right', -- "left", "right", "top", "bottom", "float", "current"
+        width = 25,
       },
-    })
+    }
   end,
 }
