04d6b66a844f871e4fe2ec3fa944835c02909980
Brendan Coen <brendan@sonic.net>
2026-01-10 18:19:33 -0800

    resovled conflicts

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,20 +1,4 @@
 return {
-<<<<<<< HEAD
-  "ray-x/go.nvim",
-  dependencies = {  -- optional packages
-    "ray-x/guihua.lua",
-    "neovim/nvim-lspconfig",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  opts = function()
-
-    require("go").setup(opts)
-    local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      pattern = "*.go",
-      callback = function()
-      require('go.format').goimports()
-=======
   'ray-x/go.nvim',
   dependencies = { -- optional packages
     'ray-x/guihua.lua',
@@ -28,7 +12,6 @@ return {
       pattern = '*.go',
       callback = function()
         require('go.format').goimports()
->>>>>>> refs/remotes/origin/master
       end,
       group = format_sync_grp,
     })
@@ -37,13 +20,7 @@ return {
       -- other options
     }
   end,
-<<<<<<< HEAD
-  event = {"CmdlineEnter"},
-  ft = {"go", 'gomod'},
-  build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
-=======
   event = { 'CmdlineEnter' },
   ft = { 'go', 'gomod' },
   build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
->>>>>>> refs/remotes/origin/master
 }
